e8d48c6aac7f0d8f8dae553b6001e812be198953
Renee Li <renee.li@fivetran.com>
2024-09-03 13:02:03 -0400

    add conversion fields to consistency pin test

--- integration_tests/tests/consistency/consistency_pin_promotion_report.sql
+++ integration_tests/tests/consistency/consistency_pin_promotion_report.sql
@@ -8,7 +8,10 @@ with prod as (
         pin_promotion_id,
         sum(clicks) as clicks, 
         sum(impressions) as impressions,
-        sum(spend) as spend
+        sum(spend) as spend,
+        sum(total_conversions) as total_conversions,
+        sum(total_conversions_quantity) as total_conversions_quantity,
+        sum(total_conversions_value_in_micro_dollar) as total_conversions_value_in_micro_dollar
     from {{ target.schema }}_pinterest_ads_prod.pinterest_ads__pin_promotion_report
     group by 1
 ),
@@ -18,7 +21,10 @@ dev as (
         pin_promotion_id,
         sum(clicks) as clicks, 
         sum(impressions) as impressions,
-        sum(spend) as spend
+        sum(spend) as spend,
+        sum(total_conversions) as total_conversions,
+        sum(total_conversions_quantity) as total_conversions_quantity,
+        sum(total_conversions_value_in_micro_dollar) as total_conversions_value_in_micro_dollar
     from {{ target.schema }}_pinterest_ads_dev.pinterest_ads__pin_promotion_report
     group by 1
 ),
@@ -31,7 +37,13 @@ final as (
         prod.impressions as prod_impressions,
         dev.impressions as dev_impressions,
         prod.spend as prod_spend,
-        dev.spend as dev_spend
+        dev.spend as dev_spend,
+        prod.total_conversions as prod_total_conversions,
+        dev.total_conversions as dev_total_conversions,
+        prod.total_conversions_quantity as prod_total_conversions_quantity,
+        dev.total_conversions_quantity as dev_total_conversions_quantity,
+        prod.total_conversions_value_in_micro_dollar as prod_total_conversions_value_in_micro_dollar,
+        dev.total_conversions_value_in_micro_dollar as dev_total_conversions_value_in_micro_dollar
     from prod
     full outer join dev 
         on dev.pin_promotion_id = prod.pin_promotion_id
